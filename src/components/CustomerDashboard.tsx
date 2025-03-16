10c756979e3433c9803787f5d72ad432037c7a4c
David Alejandro González Robles <66448264+Davidgr11@users.noreply.github.com>
2025-03-15 18:39:15 -0600

    Update CustomerDashboardSupa.tsx

--- src/components/CustomerDashboard.tsx
+++ src/components/CustomerDashboard.tsx
@@ -166,7 +166,6 @@ const CustomerDashboard = () => {
             className="mt-4 md:mt-0"
             onClick={() => {
               logout();
-              navigate('/login');
             }}
           >
             Logout
